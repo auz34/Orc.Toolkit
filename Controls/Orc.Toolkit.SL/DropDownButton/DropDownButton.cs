2d2a09fa140992370daf879857eb2f8383077d50
Orcomp <Orcomp@server>
2013-02-16 11:17:15 +1100

    Re-included missing assemblie references

--- Controls/Orc.Toolkit.SL/DropDownButton/DropDownButton.cs
+++ Controls/Orc.Toolkit.SL/DropDownButton/DropDownButton.cs
@@ -9,8 +9,17 @@
 
 namespace Orc.Toolkit
 {
+    ﻿using System;
+    using System.Net;
     using System.Windows;
+    using System.Windows.Controls;
     using System.Windows.Controls.Primitives;
+    using System.Windows.Documents;
+    using System.Windows.Ink;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Shapes;
 
     /// <summary>
     /// The drop down button.
